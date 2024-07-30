6aaeee9186e91261eda618b8ceeae641ed145322
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-30 09:42:08 -0600

    feat: create connection with mongoose

--- Backend/db.js
+++ Backend/db.js
@@ -1,18 +1,14 @@
-export default async function connectDB(client, uri) {
+import { connect } from "mongoose";
+
+export default async function connectDB() {
   try {
     console.log("Connecting to MongoDB...");
-    console.log("URI: ", uri);
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const URI = process.env.MONGO_URI;
+    await connect(URI);
+    console.log("Connected to MongoDB!");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
-    throw error;
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    process.exit(1);
   }
 }
 
