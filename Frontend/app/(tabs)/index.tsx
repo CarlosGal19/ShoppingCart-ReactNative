22f629bfff40c350de79df61bae8a6f3dbe66f9b
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-30 13:29:13 -0600

    feat: create layouts and routes

---
+++ Frontend/app/(tabs)/index.tsx
@@ -0,0 +1,76 @@
+import { FC } from "react";
+import { View, Text, ActivityIndicator, FlatList, StyleSheet } from "react-native";
+import axiosClient from "../../axios/axios";
+import { useEffect, useState } from "react";
+
+import ProductCard from "../../components/Product";
+
+interface Product {
+    _id: string;
+    name: string;
+    price: number;
+    image: string;
+    countInStock: number;
+}
+
+const App: FC = () => {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [products, setProducts] = useState<Product[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axiosClient.get("/");
+                setProducts(response.data.products);
+            } catch (error) {
+                setError("Failed to fetch products. Please try again later.");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
+
+    if (error) {
+        return (
+            <View style={styles.container}>
+                <Text>{error}</Text>
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            {products.length > 0 ? (
+                <FlatList
+                    data={products}
+                    renderItem={({ item }) => <ProductCard key={item._id} product={item} />}
+                    keyExtractor={item => item._id}
+                />
+            ) : (
+                <Text>No products available.</Text>
+            )}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // Ensures the container takes up the full screen
+        justifyContent: 'center', // Centers content vertically
+        alignItems: 'center', // Centers content horizontally
+        padding: 10, // Adds padding around the container
+        backgroundColor: '#959595'
+    },
+});
+
+export default App;
