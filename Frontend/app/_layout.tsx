0b14332a5ba993241b981ce307eefae1a14018cf
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-31 09:19:17 -0600

    refactor: remove provider and context

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -1,43 +1,43 @@
 import { View } from "react-native";
 import { Stack } from "expo-router";
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import {
+    SafeAreaProvider,
+    useSafeAreaInsets,
+} from "react-native-safe-area-context";
 import { CartProvider } from "../Context/CartProvider";
-import { AmountProvider } from "../Context/AmountProvider";
 
 export default function App() {
-
     const insets = useSafeAreaInsets();
 
     return (
         <>
             <CartProvider>
-                <AmountProvider>
-                    <SafeAreaProvider >
-                        <StatusBar />
-                        <View
-                            style={{
-                                flex: 1,
-                                paddingBottom: insets.bottom,
-                                paddingLeft: insets.left,
-                                paddingRight: insets.right,
-                                paddingTop: insets.top,
-                            }}>
-                            < Stack
-                                screenOptions={{
-                                    headerStyle: {
-                                        backgroundColor: 'black'
-                                    },
-                                    headerTintColor: 'white',
-                                    headerTitleStyle: {
-                                        fontWeight: 'bold',
-                                    },
-                                    headerTitle: 'Shopping Cart',
-                                }}
-                            />
-                        </View>
-                    </SafeAreaProvider >
-                </AmountProvider>
+                <SafeAreaProvider>
+                    <StatusBar />
+                    <View
+                        style={{
+                            flex: 1,
+                            paddingBottom: insets.bottom,
+                            paddingLeft: insets.left,
+                            paddingRight: insets.right,
+                            paddingTop: insets.top,
+                        }}
+                    >
+                        <Stack
+                            screenOptions={{
+                                headerStyle: {
+                                    backgroundColor: "black",
+                                },
+                                headerTintColor: "white",
+                                headerTitleStyle: {
+                                    fontWeight: "bold",
+                                },
+                                headerTitle: "Shopping Cart",
+                            }}
+                        />
+                    </View>
+                </SafeAreaProvider>
             </CartProvider>
         </>
     );
