403583cc02c1c82def8610249c78817e3e50aa09
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-30 21:35:14 -0600

    feat: use provider

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { CartProvider } from "../Context/CartProvider";
+import { AmountProvider } from "../Context/AmountProvider";
 
 export default function App() {
 
@@ -11,30 +12,32 @@ export default function App() {
     return (
         <>
             <CartProvider>
-                <SafeAreaProvider >
-                    <StatusBar />
-                    <View
-                        style={{
-                            flex: 1,
-                            paddingBottom: insets.bottom,
-                            paddingLeft: insets.left,
-                            paddingRight: insets.right,
-                            paddingTop: insets.top,
-                        }}>
-                        < Stack
-                            screenOptions={{
-                                headerStyle: {
-                                    backgroundColor: 'black'
-                                },
-                                headerTintColor: 'white',
-                                headerTitleStyle: {
-                                    fontWeight: 'bold',
-                                },
-                                headerTitle: 'Shopping Cart',
-                            }}
-                        />
-                    </View>
-                </SafeAreaProvider >
+                <AmountProvider>
+                    <SafeAreaProvider >
+                        <StatusBar />
+                        <View
+                            style={{
+                                flex: 1,
+                                paddingBottom: insets.bottom,
+                                paddingLeft: insets.left,
+                                paddingRight: insets.right,
+                                paddingTop: insets.top,
+                            }}>
+                            < Stack
+                                screenOptions={{
+                                    headerStyle: {
+                                        backgroundColor: 'black'
+                                    },
+                                    headerTintColor: 'white',
+                                    headerTitleStyle: {
+                                        fontWeight: 'bold',
+                                    },
+                                    headerTitle: 'Shopping Cart',
+                                }}
+                            />
+                        </View>
+                    </SafeAreaProvider >
+                </AmountProvider>
             </CartProvider>
         </>
     );
